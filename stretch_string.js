33973d6443ca8be70120e67a07f9b80985b3292f
Michael Stone <mikepwns37@gmail.com>
2018-01-24 19:42:01 -0500

    Add comments to stretch_string.js

--- stretch_string.js
+++ stretch_string.js
@@ -1,12 +1,16 @@
+// Example user input
 var input = 'hello';
 var integers = [2, 4, 6, 8, 10];
 
+// Get user-inputted string
 console.log('Enter a string: \n');
 
+// Prompt user to enter number of strings depending on length of input
 console.log('Enter ' + input.length + ' integers: \n');
 
 console.log('Printing stretched string: \n');
 
+// 'Stretch' user's string depending on integer input
 for (var i = 0; i < input.length; ++i) {
 	for (var j = 0; j < integers[i]; ++j) {
 		console.log(input[i]);
